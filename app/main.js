de8df59f298eb73dde8fd96976a35e390527e843
Alice Emily <alice.nascimento@bsatech.io>
2026-06-03 01:58:19 -0300

    feat: add description and images from events

--- app/main.js
+++ app/main.js
@@ -11,3 +11,37 @@ function enviarMensagem(event) {
 
         window.open(url, '_blank')
     }
+
+let eventoAtual = 0;
+const eventos = document.querySelectorAll(".evento-card");
+
+function mostrarEvento(index) {
+    eventos.forEach(evento => evento.classList.remove("ativo"));
+
+    if (index >= eventos.length) {
+        eventoAtual = 0;
+    } else if (index < 0) {
+        eventoAtual = eventos.length - 1;
+    } else {
+        eventoAtual = index;
+    }
+
+    eventos[eventoAtual].classList.add("ativo");
+}
+
+function mudarEvento(direcao) {
+    mostrarEvento(eventoAtual + direcao);
+}
+
+setInterval(() => {
+    const eventoAtivo = document.querySelector(".evento-card.ativo");
+    const fotos = eventoAtivo.querySelectorAll(".foto-evento");
+
+    let fotoAtual = Array.from(fotos).findIndex(foto => foto.classList.contains("ativa"));
+
+    fotos[fotoAtual].classList.remove("ativa");
+
+    fotoAtual = (fotoAtual + 1) % fotos.length;
+
+    fotos[fotoAtual].classList.add("ativa");
+}, 2000);
